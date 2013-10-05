f3035dae2bedd67f9f9a4e4bac66501edbf28119
LindsayYoung <lindsay.n.young@gmail.com>
2013-10-05 14:57:49 -0400

    getting definitions from statedecoded

--- scripts/migrate_definitions.py
+++ scripts/migrate_definitions.py
@@ -4,12 +4,13 @@ r = requests.get("https://raw.github.com/statedecoded/legal-dictionary/master/di
 
 content = r.json()
 for l in content:
-	term = l['term']
+	term = str(l['term'])
+	term = term[0].upper() + term[1:]
 	definition = l['definition']
 	url = l['source_url']
 	source = l['source']
 	
-	copy = "%s \n\nSource:[%s](%s)" % (definition, source, url)
+	copy = "%s \n\nSource: [%s](%s)" % (definition, source, url)
 	copy = copy.encode('ascii', 'ignore')
 	
 	file_name = "/Users/lindsayyoung/Glossary/definitions/" + term
