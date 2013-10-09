0dfccc868385871eca2c02fe3f125fb5c6157df1
Eric Mill <eric@konklone.com>
2013-10-08 20:09:22 -0400

    testing out payload things

--- web.js
+++ web.js
@@ -10,53 +10,38 @@ app.get('/', function(request, response) {response.send('Hello World!');});
 
 
 app.post('/', function(request, response) {
-	console.log("request = " + util.inspect(request.body));
 	if (!request || !request.body || !request.body.payload) {
 		console.log("No payload specified.");
 		return;
-	};
+	}
 
 	var payload = JSON.parse(request.body.payload);
-	console.log("payload = " + util.inspect(payload));
-	console.log("commits length = " + payload.commits.length);
+	console.log(util.inspect(payload));
 
-	var masterBranchBaseUrl = "https://raw.github.com/keith5000/USGlossary/";
+	// only care about commits to the master branch
+	var branch = payload.ref.split("/").slice(-1)[0];
+	if (branch != "master")
+		return;
 
-	for (var commitCount=0; commitCount < payload.commits.length; commitCount++) {
-		if (payload.commits.added) {
-			for (var addedCount=0; addedCount < payload.commits.added.length; addedCount++) {
-				var rawFileUrl = masterBranchBaseUrl + payload.commits.added[addedCount];
-				console.log("Added url: " + rawFileUrl);
-			}
-		}
+	// var masterBranchBaseUrl = "https://raw.github.com/keith5000/USGlossary/";
 
-		if (payload.commits.modified) {
-			for (var modifiedCount=0; modifiedCount < payload.commits.modified.length; modifiedCount++) {
-				var rawFileUrl = masterBranchBaseUrl + payload.commits.modified[modifiedCount];
-				console.log("Modified url: " + rawFileUrl);
-			}
-		}
-	}
+	// for (var commitCount=0; commitCount < payload.commits.length; commitCount++) {
+	// 	if (payload.commits.added) {
+	// 		for (var addedCount=0; addedCount < payload.commits.added.length; addedCount++) {
+	// 			var rawFileUrl = masterBranchBaseUrl + payload.commits.added[addedCount];
+	// 			console.log("Added url: " + rawFileUrl);
+	// 		}
+	// 	}
+
+	// 	if (payload.commits.modified) {
+	// 		for (var modifiedCount=0; modifiedCount < payload.commits.modified.length; modifiedCount++) {
+	// 			var rawFileUrl = masterBranchBaseUrl + payload.commits.modified[modifiedCount];
+	// 			console.log("Modified url: " + rawFileUrl);
+	// 		}
+	// 	}
+	// }
 
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
-
-
-/*
-var githubhook = require('githubhook');
-var github = githubhook({ port: process.env.PORT || 3240 });
-
-github.listen();
-
-github.on('push:https://github.com/keith5000/USGlossary/tree/CommitTest', function (ref, data) {
-	response.send("push");
-});
-
-github.on('commit:https://github.com/keith5000/USGlossary/tree/CommitTest', function (ref, data) {
-	response.send("commit");
-});
-*/
+app.listen(port, function() {console.log("Now listening on port " + port + ".");});
