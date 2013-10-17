d0e9d2c9d3a740a9f0a1ceb1213351c349d1923d
Eric Mill <eric@konklone.com>
2013-10-17 01:47:19 -0400

    plain text version of def

--- glossary.js
+++ glossary.js
@@ -1,18 +1,21 @@
 // rules for transforming glossary content from prose to data
 
-var util = require('util')
-  , marked = require('marked');
+var util = require('util'),
+    marked = require('marked'),
+    S = require('string');
 
 
 var Glossary = {
   transform: function(definition) {
     var short_definition = definition.split("\n\n")[0];
     var long_html = marked(definition);
+    var long_text = S(long_html).stripTags().s.replace(/\n/g, "\n\n");
 
     return JSON.stringify({
       short_definition: short_definition,
       long_definition_html: long_html,
-      long_definition_markdown: definition
+      long_definition_markdown: definition,
+      long_definition_text: long_text
     });
   }
 };
