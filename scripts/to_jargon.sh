ef622f1f731abd7ba2f3e32673af97e4aa36a18b
Paul Tagliamonte <tag@pault.ag>
2015-04-29 20:06:25 -0400

    Add in a converter to the Jargon File-like format
      This is commonly used with other dictionaries too, and there are
      some neato parsers out there.

---
+++ scripts/to_jargon.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+DICT_SET=$1
+
+if [ "x${DICT_SET}" = "x" ]; then
+    echo "Euch! Shucks! I need a set to process (in definitions/)"
+    echo ""
+    echo "Valid sets:"
+    echo "  $(ls definitions/ | tr "\n" " ")"
+    exit 1
+fi
+
+find definitions -type f | while read x; do
+    echo ":$(basename ${x}):"
+    fmt "${x}" | sed 's/^/  /g'
+    echo ""
+done
